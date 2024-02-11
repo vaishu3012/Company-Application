aa3d03f8af391686372d36c4af13189dc552b797
vaishnavi sabale <121756858+vaishu3012@users.noreply.github.com>
2024-02-11 09:57:14 +0530

    Add files via upload

---
+++ CompanyapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.jspiders.companyapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyapplicationApplication.class, args);
+	}
+
+}
---
+++ CompanyapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jspiders.companyapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
